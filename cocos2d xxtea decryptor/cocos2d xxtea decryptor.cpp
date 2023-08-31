0faace959f0fa375b93e3235fed02f0529aa7924
dragonchinlo <38606611+lambwheit@users.noreply.github.com>
2023-09-01 09:41:39 +1000

    finished single file processing

--- cocos2d xxtea decryptor/cocos2d xxtea decryptor.cpp
+++ cocos2d xxtea decryptor/cocos2d xxtea decryptor.cpp
@@ -1,11 +1,105 @@
-// cocos2d xxtea decryptor.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// cocos2d xxtea decryptor.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include "xxtea.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <direct.h>
 
-int main()
+static std::vector<char> ReadAllBytes(char const* filename)
 {
-    std::cout << "Hello World!\n";
+    std::ifstream ifs(filename, std::ios::binary | std::ios::ate);
+    std::ifstream::pos_type pos = ifs.tellg();
+
+    if (pos == 0) {
+        return std::vector<char>{};
+    }
+
+    std::vector<char>  result(pos);
+
+    ifs.seekg(0, std::ios::beg);
+    ifs.read(result.data(), pos);
+
+    return result;
+}
+
+int isDir(char* path)
+{
+	struct stat s;
+	if( stat(path,&s) == 0 )
+	{
+		if( s.st_mode & S_IFDIR )
+		{
+			return 1;
+		}
+		else if( s.st_mode & S_IFREG )
+		{
+			return 0;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+int main(int argc, char* argv[]){
+	if(argc != 4)
+	{
+		printf("Usage: %s [key] [sign] [input file/dir]\n", std::string(argv[0]).substr(std::string(argv[0]).find_last_of("/\\") + 1).c_str());
+		return -1;
+	}
+	char* key = argv[1];
+	char* sign = argv[2];
+	char* path = argv[3];
+	const int res = isDir(path);
+	if(res == 1)
+	{
+		
+	}
+	else if(res == 0)
+	{
+		std::vector<char> data = ReadAllBytes(path);
+		data.erase(data.begin(), data.begin() + static_cast<long>(strlen(sign)));
+		size_t len;
+		const char* decryptedData = static_cast<char*>(xxtea_decrypt(data.data(), data.size(), key, &len));
+		if (decryptedData != nullptr)
+		{
+			const std::string dir = std::string(path).substr(0, std::string(path).find_last_of("/\\") + 1);
+			const std::string fileName = std::string(path).substr(std::string(path).find_last_of("/\\") + 1);
+			const std::string outputPath = dir + "\out\\";
+			if(isDir(const_cast<char*>(outputPath.c_str())) == -1)
+			{
+				if(_mkdir(outputPath.c_str()) == -1)
+				{
+					std::cout << "Failed to create output directory" << std::endl;
+					return -1;
+				}
+			}
+			const std::string outputFileName = outputPath + fileName.substr(0, fileName.size() - 1);
+			std::ofstream out;
+			out.open(outputFileName);
+			out.write(decryptedData, len);
+			out.close();
+			std::cout << "Decrypted " << path << std::endl;
+		}
+		else
+		{
+			std::cout << "Failed to decrypt " << path << std::endl;
+			return -1;
+		}
+	}
+    else
+    {
+        std::cout << "Failed to process input" << std::endl;
+        return -1;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ out/ban_talk_item.lua
@@ -0,0 +1,83 @@
+--[[
+Description: 禁言tableview item
+Author: dashuaibi
+Date: 2020-08-04 15:22:28
+App: X-Poker
+--]]
+local ban_talk_item = class("ban_talk_item", base_widget)
+
+
+function ban_talk_item:ctor( params )
+	ban_talk_item.super:ctor(self, params)
+    self:set_data(params.data, params.index)
+    self.click_callback = params.click_callback
+    self:setNodeEventEnabled(true)
+
+    self.sub_node_params = {
+        head_icon = {cls = "wd_user_icon"}
+    }
+end
+
+
+function ban_talk_item:while_set_root()
+    self.head_icon = self.rootNode._children["head_icon"]
+    self.lab_name = self.rootNode._children["lab_name"]
+    self.btn_ban = self.rootNode._children["btn_ban"]
+    self.btn_unban = self.rootNode._children["btn_unban"]
+
+	self:refresh_ui()
+end
+
+
+function ban_talk_item:on_click(  )
+	local param = {
+		uid = self.data.uid,
+		ban = not self.data.is_ban_talk,
+	}
+
+    g_protocol:send_msg_BanChatC2S(param, nil, self.data.room_id)
+end
+
+function ban_talk_item:refresh_ui(  )
+    self.rootNode._children["bg"]:setVisible( self.index % 2 == 0 )
+    self.head_icon.controller:update_head_info(self.data.avatar, self.data.vip_level)
+
+    local name = g_language:get_string_by_len( self.data.name, 26, 230 )
+    self.lab_name:setString(name)
+
+    self:refresh_btn_icon()
+end
+
+function ban_talk_item:refresh_btn_icon(  )
+    self.btn_ban:setVisible(not self.data.is_ban_talk)
+    self.btn_unban:setVisible(self.data.is_ban_talk)
+end
+
+function ban_talk_item:set_data( data, index )
+    self.data = data
+    self.index = index
+end
+
+function ban_talk_item:refreshContentView( data, index )
+    self:set_data(data, index)
+    self:refresh_ui()
+end
+
+function ban_talk_item:on_update_ban_status( event )
+    local params = event.params
+	if params.uid == self.data.uid then
+		self.data.is_ban_talk = params.ban
+		self:refresh_btn_icon()
+	end
+end
+
+function ban_talk_item:addEventListeners()
+    self._updateForbidState = handler(self, self.on_update_ban_status)
+    g_event_center:addEventListener(g_custom_event.EVT_PLAYER_FORBID, self._updateForbidState)
+end
+
+function ban_talk_item:removeEventListeners()
+    g_event_center:removeEventListener(g_custom_event.EVT_PLAYER_FORBID, self._updateForbidState)
+end
+
+return ban_talk_item
